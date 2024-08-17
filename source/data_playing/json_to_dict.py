e7c861138eb97ce7306fe70b207c973117299dec
Arunangshu Pramanik <arunangshu01@gmail.com>
2024-08-17 18:00:35 +0530

    Initial Changes
    Initial Changes

---
+++ source/data_playing/json_to_dict.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+
+if __name__ == "__main__":
+
+    # Reading a json file and converting it into a dictionary
+
+    json_file_path = os.path.join('../sample_data_files', 'employee_data_json.json')
+    with open(json_file_path, 'r') as employee_json_file:
+        employee_data = json.load(employee_json_file)
+
+    print(employee_data)
